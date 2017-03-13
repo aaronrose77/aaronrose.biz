741988387e5cb302c2875bdcf6a03a8ccc1b421e
Aaron Rose <aaronrose77@gmail.com>
2017-03-13 15:50:32 -0400

    Begin structural HTML/CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,4 @@
 /*eslint-env node, mocha, browser */
-
-var x = "Hello 'my' World!";
-
-function sayHello() {
-	return x;
-}
-
 document.addEventListener("DOMContentLoaded", function(event) { 
-	console.log(sayHello());
-	//svg4everybody();
+
 });
